60c081e479bddc53b670dea1d8b16fba3123cdb8
Adnan Kazi <adnankazi0707@gmail.com>
2025-05-10 17:25:26 +0530

    Frontend Added

---
+++ backend/server.js
@@ -0,0 +1,62 @@
+// server.js
+const path = require('path')
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const eventRoutes = require('./routes/eventRoutes');
+const sportCategoryRoutes = require('./routes/sportCategoryRoutes');
+const cityRoutes = require('./routes/cityRoutes');
+const areaRoutes = require('./routes/areaRoutes');
+const publicRoutes = require('./routes/publicRoutes');
+
+// Load env
+dotenv.config();
+
+// Connect DB
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/events', eventRoutes);
+app.use('/api/admin/sport-categories', sportCategoryRoutes);
+app.use('/api/admin/cities', cityRoutes);
+app.use('/api/admin/areas', areaRoutes);
+app.use('/api/public', publicRoutes);
+
+
+// Sample route
+app.get('/', (req, res) => {
+  res.send('API is running successfully...');
+});
+
+
+
+
+// Serve static frontend from build folder
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
+
+// For any route not handled by API, serve index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'frontend', 'build', 'index.html'));
+});
+
+
+
+
+
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
